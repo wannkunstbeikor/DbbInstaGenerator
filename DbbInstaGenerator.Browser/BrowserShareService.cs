0fbffb341a15ca98aabec0242444465178664614
jona <jonathan.kopmann@gmail.com>
2025-01-03 18:48:54 +0100

    forgot

---
+++ DbbInstaGenerator.Browser/BrowserShareService.cs
@@ -0,0 +1,17 @@
+using System.IO;
+using DbbInstaGenerator.Interfaces;
+
+namespace DbbInstaGenerator.Browser;
+
+public class BrowserShareService : IShareService
+{
+    public void Share(MemoryStream inStream)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void ShareB(MemoryStream inStream)
+    {
+        throw new System.NotImplementedException();
+    }
+}
